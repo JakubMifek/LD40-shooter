1f216a3966a4f41dc9b09b9cad568d40a36f72ba
VALENTÝNKA\jakub <jakubmifek@gmail.com>
2017-12-02 15:52:43 +0100

    VR Throwing

--- Assets/Scripts/WandController.cs
+++ Assets/Scripts/WandController.cs
@@ -1,21 +1,32 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(FixedJoint)), RequireComponent(typeof(Collider))]
 public class WandController : MonoBehaviour
 {
     private SteamVR_TrackedObject trackedObj;
     private SteamVR_Controller.Device Controller { get { return SteamVR_Controller.Input((int)trackedObj.index); } }
+
+    public float rotateSpeed = 0.25f;
 
     private Valve.VR.EVRButtonId trigger = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
     public bool triggerUp = false;
     public bool triggerDown = false;
     public bool triggerPressed = false;
 
+    private GameObject pickupable;
+    private FixedJoint fJoint;
+
+    private bool throwing;
+    private Rigidbody throwable;
+
     // Use this for initialization
     void Start()
     {
         trackedObj = GetComponent<SteamVR_TrackedObject>();
+        fJoint = GetComponent<FixedJoint>();
     }
 
     // Update is called once per frame
@@ -32,9 +43,72 @@ public class WandController : MonoBehaviour
         triggerPressed = Controller.GetPress(trigger);
 
         if (triggerDown)
-            Debug.Log("Trigger down.");
+        {
+            PickUpObject();
+        }
 
         if (triggerUp)
-            Debug.Log("Trigger up.");
+        {
+            DropObject();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (throwing)
+        {
+            Transform origin;
+            if (trackedObj.origin != null)
+                origin = trackedObj.origin;
+            else
+                origin = trackedObj.transform.parent;
+
+            if (origin != null)
+            {
+                throwable.velocity = origin.TransformVector(Controller.velocity);
+                throwable.angularVelocity = origin.TransformVector(Controller.angularVelocity * rotateSpeed);
+            }
+            else
+            {
+                throwable.velocity = (Controller.velocity);
+                throwable.angularVelocity = (Controller.angularVelocity * rotateSpeed);
+            }
+
+            throwable.maxAngularVelocity = throwable.velocity.magnitude;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "pickupable")
+        {
+            pickupable = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        pickupable = null;
+    }
+
+    private void DropObject()
+    {
+        throwable = fJoint.connectedBody;
+        fJoint.connectedBody = null;
+        throwing = true;
+    }
+
+    private void PickUpObject()
+    {
+        if (pickupable != null)
+        {
+            fJoint.connectedBody = gameObject.GetComponent<Rigidbody>();
+            throwing = false;
+            throwable = null;
+        }
+        else
+        {
+            fJoint.connectedBody = null;
+        }
     }
 }
