4ded60475970a3743c131b612638ca56e2db3586
VALENTÝNKA\jakub <jakubmifek@gmail.com>
2017-12-03 10:52:26 +0100

    Hit sounds

--- Assets/Scripts/Effects/LightEffect.cs
+++ Assets/Scripts/Effects/LightEffect.cs
@@ -2,23 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody)), RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(Rigidbody)), RequireComponent(typeof(AudioSource)), RequireComponent(typeof(Collider))]
 public class LightEffect : ThrownEffect
 {
-    private AudioSource source;
     public AudioClip throwClip;
+    public AudioClip hitClip;
 
     private Rigidbody body;
+    private Collider col;
+    private AudioSource source;
+
+    private bool thrown;
 
     public override void Throw()
     {
         source.PlayOneShot(throwClip, 1.0f);
         body.AddForce(transform.forward * bonus);
+        thrown = true;
     }
 
     private void Start()
     {
         body = GetComponent<Rigidbody>();
+        col = GetComponent<Collider>();
         source = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!thrown)
+            return;
+
+        thrown = false;
+        StartCoroutine(PlaySoundAndDestroy());
+    }
+
+    private IEnumerator PlaySoundAndDestroy()
+    {
+        source.PlayOneShot(hitClip, 1.0f);
+        yield return new WaitForSeconds(hitClip.length);
+        //Destroy(gameObject);
     }
 }
