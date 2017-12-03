b896e2cd0f43ac2fbd7beaca9e8ec5ff3d776713
VALENTÝNKA\jakub <jakubmifek@gmail.com>
2017-12-03 10:13:37 +0100

    Light effect whoosh

--- Assets/Scripts/Effects/LightEffect.cs
+++ Assets/Scripts/Effects/LightEffect.cs
@@ -5,11 +5,6 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody)), RequireComponent(typeof(AudioSource))]
 public class LightEffect : ThrownEffect
 {
-
-    private bool thrown = false;
-
-    public float radius = 5.0f;
-    public float force = 2.0f;
     private AudioSource source;
     public AudioClip throwClip;
 
@@ -17,7 +12,6 @@ public class LightEffect : ThrownEffect
 
     public override void Throw()
     {
-        thrown = true;
         source.PlayOneShot(throwClip, 1.0f);
         body.AddForce(transform.forward * bonus);
     }
