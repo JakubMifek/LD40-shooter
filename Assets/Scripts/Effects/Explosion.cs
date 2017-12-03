83b0f04eff430d24f322f782612751f1f35226c2
VALENTÝNKA\jakub <jakubmifek@gmail.com>
2017-12-03 08:52:34 +0100

    Explosions

--- Assets/Scripts/Effects/Explosion.cs
+++ Assets/Scripts/Effects/Explosion.cs
@@ -2,19 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(Collider)), RequireComponent(typeof(AudioSource))]
 public class Explosion : ThrownEffect
 {
     private bool thrown = false;
 
     public float radius = 5.0f;
     public float force = 2.0f;
+    public GameObject explosion;
+    private AudioSource source;
+    public AudioClip explosionClip;
+    public AudioClip throwClip;
 
     private Collider col;
 
     public override void Throw()
     {
         thrown = true;
+        source.PlayOneShot(throwClip, 1.0f);
     }
 
     private void Start()
@@ -38,6 +43,19 @@ public class Explosion : ThrownEffect
             rb.AddExplosionForce(force * bonus, transform.position, radius * bonus, .5f, ForceMode.Impulse);
         }
 
+        var data = explosion.GetComponent<Data>();
+        if (data != null)
+            data.Size = bonus;
+
+        var expl = Instantiate(explosion, transform.position, Quaternion.identity);
+        thrown = false;
+        StartCoroutine(PlaySoundAndDestroy());
+    }
+
+    private IEnumerator PlaySoundAndDestroy()
+    {
+        source.PlayOneShot(explosionClip, 1.0f);
+        yield return new WaitForSeconds(explosionClip.length);
         Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/SetSize.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class SetSize : MonoBehaviour
+{
+
+    public Data data;
+    private ParticleSystem ps;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (data == null)
+            return;
+
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var main = ps.main;
+        main.startSizeMultiplier = data.Size;
+    }
+}
