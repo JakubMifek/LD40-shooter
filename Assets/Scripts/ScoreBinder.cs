9f43d32512b5b6f3644c11cc5abefb0c03a1beab
VALENTÝNKA\jakub <jakubmifek@gmail.com>
2017-12-03 19:28:56 +0100

    Highscore binder

---
+++ Assets/Scripts/ScoreBinder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(TextMesh))]
+public class ScoreBinder : MonoBehaviour
+{
+
+    public int index;
+
+    private TextMesh mesh;
+
+    private void Start()
+    {
+        mesh = GetComponent<TextMesh>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mesh.text = HighScore.scores[index].ToString();
+    }
+}
